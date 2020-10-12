341db92c24ec6c222fc32f4ef0319065af9eda3d
sanketh-123 <72330176+sanketh-123@users.noreply.github.com>
2020-10-12 15:46:26 +0530

    Polymorphism

---
+++ Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class Employee
+{
+    public string Firstname = "FN";
+    public string Lastname = "LN";
+    public void 
+        PrintFullname()
+    {
+        Console.WriteLine(Firstname + " " + Lastname);
+    }
+}
+public class PartTimeEmployee : Employee
+{
+}
+public class FullTimeEmployee : Employee
+{
+}
+public class TemporaryTimeEmployee : Employee
+{
+}
+public class Program
+{
+    public static void Main()
+    {
+        Employee[] employees = new Employee[4];
+
+        employees[0] = new Employee();
+        employees[1] = new PartTimeEmployee();
+        employees[2] = new FullTimeEmployee();
+        employees[3] = new TemporaryTimeEmployee();
+
+        foreach (Employee e in employees)
+        {
+            e.PrintFullname();
+        }
+    }
+}
+
+
+
+
+
