9cc4acf65231278f8ace471568fef3eb7aebca5d
sanketh-123 <72330176+sanketh-123@users.noreply.github.com>
2020-10-12 15:41:28 +0530

    Inheritance Example Program

---
+++ Inheritance/Inheritance/Class2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance 
+{
+    class Class2
+    {
+        Class2()
+        {
+            Console.WriteLine("Class2 constructor is called. ");
+
+        }
+        public void Test3()
+        {
+            Console.WriteLine("Method 3");
+        }
+
+        static void Main()
+        {
+            Class2 c = new Class2();
+            c.Test1();
+            c.Test2();
+            c.Test3();
+            Console.ReadLine();
+
+        public void Test1()
+        {
+            Console.WriteLine("Method 1");
+        }
+        public void Test2()
+        {
+            Console.WriteLine("Method 2");
+        }
+    }
+}
